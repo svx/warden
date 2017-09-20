d6b0404eeb05d81c5fd75b55ba161447b9a1cf5d
sven <sven@so36.net>
2017-09-20 13:31:01 +0200

    Add: script, update README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # warden
-Dirty Bash Wrapper Around Apt And Docker Prune - DO NOT USE IN PRODUCTION
+Dirty Bash Wrapper Around Apt And Docker Prune
+
+**DO NOT USE ON PRODUCTION SETUPS**
+
+A *dirty wrapper* around ``apt`` and ``docker`` commands.
+
+**Be aware** This script will remove **all** unused Docker Volumes, Docker Images, Docker Networks and so on.
+
+**Be sure that you know what you are doing! **
+
+## What is does
+
+Not much, it runs:
+
+* ``apt-get autoremove``
+* ``docker system prune --all --force``
+
+This script is used as *cleanup* step on a somewhat small and power (as in hardware and disk space)lacking sandbox, before ``apt updgrade``.
+
+There is the possiblity that I will combine all these steps into one
+proper *update script* in the future.
---
+++ warden.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# warden is a wrapper script around apt and docker
+# warden will:
+# - run apt-get autoremove
+# - docker prune
+
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+#COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+#COL_MAGENTA=$ESC_SEQ"35;01m"
+#COL_CYAN=$ESC_SEQ"36;01m"
+
+
+# Functions
+# Error
+error_exit() {
+    echo -en "$COL_RED$1$COL_RESET" 1>&2
+    exit 1
+}
+
+# Run apt-get autoremove
+apt_auto() {
+    echo -en "$COL_YELLOW Running apt-get autoremove ...$COL_RESET\n"
+    apt-get autoremove -y
+}
+# Docker prune
+docker_prune() {
+    echo -en "$COL_YELLOW Housekeeping docker images ...$COL_RESET\n"
+    docker system prune --all --force
+}
+
+if [[ $EUID -ne 0 ]];
+then
+    error_exit "Please run this script wit root or sudo"
+else
+    apt_auto
+    docker_prune
+fi
+
+exit 0
